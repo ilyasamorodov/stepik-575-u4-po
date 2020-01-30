6afbc9d7878c9add82e3974a0591c14b74691951
Ilya Samorodov <ilyasamorodov@gmail.com>
2020-01-31 01:50:57 +0300

    ProductPage methods added

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,16 @@
 from .base_page import BasePage
+from locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    pass
+    
+    def should_be_product_promo_link(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PROMO_LINK), "Login link is not presented"
+
+
+    def has_add_to_basket_button(self):
+        pass
+
+
+    def add_to_basket(self):
+        pass
