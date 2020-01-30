f87559d41f22e52190ef73f974197a8a1b7b67b1
Ilya Samorodov <ilyasamorodov@gmail.com>
2020-01-31 01:08:56 +0300

    Solve puzzle method added to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
+import math
 
 
 class BasePage():
@@ -7,8 +9,8 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
-    
-    
+
+
     def open(self):
         self.browser.get(self.url)
 
@@ -23,3 +25,17 @@ class BasePage():
     
     def is_current_url_valid(self, url):
         return url in self.browser.current_url
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
