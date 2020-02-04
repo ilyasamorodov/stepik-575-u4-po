d06985d317bcca1c9a93f5ad1c00557428d7cc0b
Ilya Samorodov <ilyasamorodov@gmail.com>
2020-02-05 00:48:05 +0300

    test_guest_can_add_product_to_basket input parameters fix

--- test_product_page.py
+++ test_product_page.py
@@ -91,7 +91,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
 
 @pytest.mark.need_review
-def test_guest_can_add_product_to_basket(self, setup, browser):
+def test_guest_can_add_product_to_basket(browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
