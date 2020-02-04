26b49d0ab144228eb966559302c17a5941a7e6b9
Ilya Samorodov <ilyasamorodov@gmail.com>
2020-02-04 23:08:37 +0300

    Moving tests to UserAddToBasketFromProductPage class

--- test_product_page.py
+++ test_product_page.py
@@ -4,17 +4,24 @@ from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
 
 
-@pytest.mark.skip
-@pytest.mark.parametrize('offer_number', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
-def test_guest_can_add_product_to_basket(browser, offer_number):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + str(offer_number)
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_product_page()
-    page.add_to_basket()
-    page.solve_quiz_and_get_code()
-    page.success_message_contains_product_title()
-    assert page.get_product_price() in page.get_basket_total_price(), "Product and basket prices are not equal!"
+class TestUserAddToBasketFromProductPage():
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_be_product_page()
+        assert product_page.has_no_success_message(), "Unexpected success message found!"
+
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_product_page()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.success_message_contains_product_title()
+        assert page.get_product_price() in page.get_basket_total_price(), "Product and basket prices are not equal!"
 
 
 @pytest.mark.xfail
@@ -25,14 +32,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page.should_be_product_page()
     product_page.add_to_basket()
     product_page.solve_quiz_and_get_code()
-    assert product_page.has_no_success_message(), "Unexpected success message found!"
-
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_product_page()
     assert product_page.has_no_success_message(), "Unexpected success message found!"
 
 
